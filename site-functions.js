24c45092d006cf1b8a248007bb45ab90e253c743
Ashley Lowde <ashley.lowde@gmail.com>
2014-01-06 17:00:05 +1030

    Moved stable functions to external script, added sanitisation for regex creation

--- site-functions.js
+++ site-functions.js
@@ -26,8 +26,8 @@ function renderNew(numRows) {
                 $("#row" + recordSet[i].ID).append("<td id=\"" + id + "\">" + value + "</td>");
             }
         });
-        $("#row" + recordSet[i].ID).append("<td class=\"upArrow\" id=\"up" + recordSet[i].ID + "\">&nbsp&nbsp</td>" +
-            "<td class=\"downArrow\" id=\"down" + recordSet[i].ID + "\">&nbsp&nbsp</td>");
+        //$("#row" + recordSet[i].ID).append("<td class=\"upArrow\" id=\"up" + recordSet[i].ID + "\">&nbsp&nbsp</td>" +
+        //    "<td class=\"downArrow\" id=\"down" + recordSet[i].ID + "\">&nbsp&nbsp</td>");
     }
 }
 
@@ -102,8 +102,8 @@ function renderOld(numRows) {
                 $("#row" + recordSet[i].ID).append("<td id=\"" + id + "\">" + value + "</td>");
             }
         });
-        $("#row" + recordSet[i].ID).append("<td class=\"upArrow\" id=\"up" + recordSet[i].ID + "\">&nbsp&nbsp</td>" + // Add a cell
-            "<td class=\"downArrow\" id=\"down" + recordSet[i].ID + "\">&nbsp&nbsp</td>"); // Add a cell
+        //$("#row" + recordSet[i].ID).append("<td class=\"upArrow\" id=\"up" + recordSet[i].ID + "\">&nbsp&nbsp</td>" + // Add a cell
+        //    "<td class=\"downArrow\" id=\"down" + recordSet[i].ID + "\">&nbsp&nbsp</td>"); // Add a cell
 
     }
 }
@@ -132,8 +132,8 @@ function initialiseTable() {
                                 $("#row" + recordSet[key].ID).append("<td id=\"" + id + "\">" + value + "</td>");
                             }
                         });
-                        $("#row" + recordSet[key].ID).append("<td class=\"upArrow\" id=\"up" + recordSet[key].ID + "\">&nbsp&nbsp</td>" +
-                            "<td class=\"downArrow\" id=\"down" + recordSet[key].ID + "\">&nbsp&nbsp</td>");
+                        //$("#row" + recordSet[key].ID).append("<td class=\"upArrow\" id=\"up" + recordSet[key].ID + "\">&nbsp&nbsp</td>" +
+                        //    "<td class=\"downArrow\" id=\"down" + recordSet[key].ID + "\">&nbsp&nbsp</td>");
 
                     });
                 }
@@ -154,3 +154,90 @@ function selectionClear(columnID){
 
 
 }
+
+ function selectionCommit(updownvote) {
+
+        var regexToCommit = {       // We create the regex with the default pattern for each field, [match all]
+                datetime: '.*',
+                host:     '.*',
+                facility: '.*',
+                pid:      '.*',
+                program:  '.*',
+                message:  '.*',
+                priority: updownvote
+        };
+
+    if (updownvote == '1') {
+        $('#upArrow').height(80);
+        $('#upArrow').css('background-size:', 'auto, 80px');
+        console.log("Upsized");
+    };
+
+        $("span:first-child").each(function(){                  // For each <span> element that's the first <span> in it's cell
+                                                                // I.e., for each cell with a span element
+            var cellHTML = this.parentNode.innerHTML;           // Grab the cell HTML
+            cellHTML.replace(/([\.\\\+\*\?\[\^\]\$\(\)])/g, '\\$1');    // Escape any regex characters that will mess with us later
+            if (cellHTML.match(/^<span class="selection">/)) {                  // If the selected text starts at line start
+                cellHTML = cellHTML.replace(/^<span class="selection">/,'^');   // Just remove the first tag
+            } else {
+                cellHTML = cellHTML.replace(/^.*?<span class="selection">/,'^.*'); // Otherwise remove non-selected text from line start to the first span tag, including the tag
+            }
+            while (cellHTML.match(/<span class="selection">/)){
+                cellHTML = cellHTML.replace(/<\/span>.*?<span class="selection">/,'.*'); // As above for each middle tag
+            }
+            cellHTML = cellHTML.replace(/<\/span>.*$/,'.*');                  // And again for the final tag
+            switch (this.parentNode.attributes['id'].value){
+                case "datetime":
+                    regexToCommit.datetime = cellHTML;
+                    break;
+                case "host":
+                    regexToCommit.host = cellHTML;
+                    break;
+                case "facility":
+                    regexToCommit.facility = cellHTML;
+                    break;
+                case "pid":
+                    regexToCommit.pid = cellHTML;
+                    break;
+                case "program":
+                    regexToCommit.program = cellHTML;
+                    break;
+                case "message":
+                    regexToCommit.message = cellHTML;
+                    break;
+                default:
+                    // Something fishy is happening - shouldn't have a <td> with different ID
+                    console.log("Damnit, Unhygienix - value shouldn't be: "+ rangeObj.startContainer.attributes['id'].value);
+            } // Using a switch statement to avoid passing a DOM ID directly
+        });
+
+        regexToCommit.command = "regexAdd";
+
+        $.getJSON("response.php", regexToCommit, function (response) {
+                    console.log(response);
+                }
+        );
+
+    }
+
+    function selectionAdd() {
+
+        var workingRowId;
+        var rangeObj;                            // The rangeObj object is used specifically when manipulating selected text, can't do it directly
+        var selection = window.getSelection ();  // Get the current selected text
+        if (!(selection.toString())) {  return;}  // If called when nothing is selected, exit immediately
+        rangeObj = selection.getRangeAt(0);      // Create a range object from the selection
+        if (rangeObj.endContainer != rangeObj.startContainer) {rangeObj.setEndAfter(rangeObj.startContainer);} // If we have multiple cells selected, adjust the range to only the first cell
+        workingRowId = rangeObj.startContainer.parentNode.parentNode.firstChild.innerHTML;
+        if (workingRowId != currentRegex.ID) {   // Confirm we're still highlighting the same row
+            selectionClear();
+            currentRegex.ID = workingRowId;
+        }
+        var span = document.createElement("span"); // Create a span tag so we can highlight the text for cosmetic purposes
+        span.className="selection";              // Set the span to the highlight class
+        rangeObj.surroundContents(span);         // Put the span tag on the selected text
+        if (rangeObj.startContainer.attributes['id'].value === "ID"){
+            selectionClear();
+        }
+     }
+
